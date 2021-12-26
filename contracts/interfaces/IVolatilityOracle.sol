5b3bc567a2c5bb372c78d1a94e03982dcb9b1843
Hayden Shively <17186559+haydenshively@users.noreply.github.com>
2021-12-26 12:48:59 -0800

    Add Aloe flag to VolatilityOracle and update actions yml (#2)

--- contracts/interfaces/IVolatilityOracle.sol
+++ contracts/interfaces/IVolatilityOracle.sol
@@ -4,7 +4,17 @@ pragma solidity ^0.8.10;
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 interface IVolatilityOracle {
-    function cachedPoolMetadata(address)
+    /**
+     * @notice Accesses the most recently stored metadata for a given Uniswap pool
+     * @dev These values may or may not have been initialized and may or may not be
+     * up to date. `tickSpacing` will be non-zero if they've been initialized.
+     * @param pool The Uniswap pool for which metadata should be retrieved
+     * @return maxSecondsAgo The age of the oldest observation in the pool's oracle
+     * @return gamma0 The pool fee minus the protocol fee on token0, scaled by 1e6
+     * @return gamma1 The pool fee minus the protocol fee on token1, scaled by 1e6
+     * @return tickSpacing The pool's tick spacing
+     */
+    function cachedPoolMetadata(address pool)
         external
         view
         returns (
@@ -13,6 +23,21 @@ interface IVolatilityOracle {
             uint24 gamma1,
             int24 tickSpacing
         );
+
+    /**
+     * @notice Accesses any of the 25 most recently stored fee growth structs
+     * @dev The full array (idx=0,1,2...24) has data that spans *at least* 24 hours
+     * @param pool The Uniswap pool for which fee growth should be retrieved
+     * @param idx The index into the storage array
+     * @return feeGrowthGlobal0X128 Total pool revenue in token0, as of timestamp
+     * @return feeGrowthGlobal1X128 Total pool revenue in token1, as of timestamp
+     * @return timestamp The time at which snapshot was taken and stored
+     */
+    function feeGrowthGlobals(address pool, uint256 idx) external view returns (
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128,
+        uint32 timestamp
+    );
 
     function estimate24H(
         IUniswapV3Pool pool,
--- contracts/libraries/LiquidityAmounts.sol
+++ contracts/libraries/LiquidityAmounts.sol
@@ -27,7 +27,7 @@ library LiquidityAmounts {
     ) internal pure returns (uint128 liquidity) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
         uint256 intermediate = FullMath.mulDiv(sqrtRatioAX96, sqrtRatioBX96, FixedPoint96.Q96);
-        return toUint128(FullMath.mulDiv(amount0, intermediate, sqrtRatioBX96 - sqrtRatioAX96));
+        liquidity = toUint128(FullMath.mulDiv(amount0, intermediate, sqrtRatioBX96 - sqrtRatioAX96));
     }
 
     /// @notice Computes the amount of liquidity received for a given amount of token1 and price range
@@ -42,7 +42,7 @@ library LiquidityAmounts {
         uint256 amount1
     ) internal pure returns (uint128 liquidity) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
-        return toUint128(FullMath.mulDiv(amount1, FixedPoint96.Q96, sqrtRatioBX96 - sqrtRatioAX96));
+        liquidity = toUint128(FullMath.mulDiv(amount1, FixedPoint96.Q96, sqrtRatioBX96 - sqrtRatioAX96));
     }
 
     /// @notice Computes the maximum amount of liquidity received for a given amount of token0, token1, the current
@@ -86,14 +86,13 @@ library LiquidityAmounts {
     ) internal pure returns (uint224 amount0) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
 
-        return
-            uint64(
-                FullMath.mulDiv(
-                    uint256(liquidity) << FixedPoint96.RESOLUTION,
-                    sqrtRatioBX96 - sqrtRatioAX96,
-                    sqrtRatioBX96
-                ) / sqrtRatioAX96
-            );
+        amount0 = uint64(
+            FullMath.mulDiv(
+                uint256(liquidity) << FixedPoint96.RESOLUTION,
+                sqrtRatioBX96 - sqrtRatioAX96,
+                sqrtRatioBX96
+            ) / sqrtRatioAX96
+        );
     }
 
     /// @notice Computes the amount of token1 for a given amount of liquidity and a price range
@@ -108,7 +107,7 @@ library LiquidityAmounts {
     ) internal pure returns (uint192 amount1) {
         if (sqrtRatioAX96 > sqrtRatioBX96) (sqrtRatioAX96, sqrtRatioBX96) = (sqrtRatioBX96, sqrtRatioAX96);
 
-        return uint192(FullMath.mulDiv(liquidity, sqrtRatioBX96 - sqrtRatioAX96, FixedPoint96.Q96));
+        amount1 = uint192(FullMath.mulDiv(liquidity, sqrtRatioBX96 - sqrtRatioAX96, FixedPoint96.Q96));
     }
 
     /// @notice Computes the token0 and token1 value for a given amount of liquidity, the current
