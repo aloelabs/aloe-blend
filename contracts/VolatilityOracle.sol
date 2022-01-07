967941ede3f710213376998b66f85d221963be15
hayden Shively <haydenshively@gmail.com>
2022-01-07 17:10:00 -0600

    Optimize VolatilityOracle and add test suite

--- contracts/VolatilityOracle.sol
+++ contracts/VolatilityOracle.sol
@@ -6,20 +6,47 @@ import "./libraries/Volatility.sol";
 
 import "./interfaces/IVolatilityOracle.sol";
 
+/*
+                              #                                                                    
+                             ###                                                                   
+                             #####                                                                 
+          #                 #######                                *###*                           
+           ###             #########                         ########                              
+           #####         ###########                   ###########                                 
+           ########    ############               ############                                     
+            ########    ###########         *##############                                        
+           ###########   ########      #################                                           
+           ############   ###      #################                                               
+           ############       ##################                                                   
+          #############    #################*         *#############*                              
+         ##############    #############      #####################################                
+        ###############   ####******      #######################*                                 
+      ################                                                                             
+    #################   *############################*                                             
+      ##############    ######################################                                     
+          ########    ################*                     **######*                              
+              ###    ###                                                                           
+*/
+
 contract VolatilityOracle is IVolatilityOracle {
+    /// @inheritdoc IVolatilityOracle
     mapping(address => Volatility.PoolMetadata) public cachedPoolMetadata;
 
-    mapping(address => Volatility.FeeGrowthGlobals[26]) public feeGrowthGlobals;
+    /// @inheritdoc IVolatilityOracle
+    mapping(address => Volatility.FeeGrowthGlobals[25]) public feeGrowthGlobals;
 
+    /// @inheritdoc IVolatilityOracle
     mapping(address => uint8) public feeGrowthGlobalsReadIndex;
 
+    /// @inheritdoc IVolatilityOracle
     mapping(address => uint8) public feeGrowthGlobalsWriteIndex;
 
+    /// @inheritdoc IVolatilityOracle
     function cacheMetadataFor(IUniswapV3Pool pool) external {
         Volatility.PoolMetadata memory poolMetadata;
 
         (, , uint16 observationIndex, uint16 observationCardinality, , uint8 feeProtocol, ) = pool.slot0();
-        poolMetadata.oldestObservation = Oracle.getOldestObservation(pool, observationIndex, observationCardinality);
+        poolMetadata.maxSecondsAgo = Oracle.getMaxSecondsAgo(pool, observationIndex, observationCardinality);
 
         uint24 fee = pool.fee();
         poolMetadata.gamma0 = fee;
@@ -32,12 +59,13 @@ contract VolatilityOracle is IVolatilityOracle {
         cachedPoolMetadata[address(pool)] = poolMetadata;
     }
 
+    /// @inheritdoc IVolatilityOracle
     function estimate24H(
         IUniswapV3Pool pool,
         uint160 sqrtPriceX96,
         int24 tick
     ) external returns (uint256 IV) {
-        Volatility.FeeGrowthGlobals[26] memory feeGrowthGlobal = feeGrowthGlobals[address(pool)];
+        Volatility.FeeGrowthGlobals[25] memory feeGrowthGlobal = feeGrowthGlobals[address(pool)];
         uint8 readIndex = _pickReadIndex(pool, feeGrowthGlobal);
 
         Volatility.FeeGrowthGlobals memory current;
@@ -47,8 +75,7 @@ contract VolatilityOracle is IVolatilityOracle {
         feeGrowthGlobalsReadIndex[address(pool)] = readIndex;
         uint8 writeIndex = feeGrowthGlobalsWriteIndex[address(pool)];
         if (current.timestamp - 1 hours > feeGrowthGlobal[writeIndex].timestamp) {
-            writeIndex = (writeIndex + 1) % 26;
-
+            writeIndex = (writeIndex + 1) % 25;
             feeGrowthGlobals[address(pool)][writeIndex] = current;
             feeGrowthGlobalsWriteIndex[address(pool)] = writeIndex;
         }
@@ -62,7 +89,7 @@ contract VolatilityOracle is IVolatilityOracle {
     ) private view returns (uint256 IV, Volatility.FeeGrowthGlobals memory current) {
         Volatility.PoolMetadata memory poolMetadata = cachedPoolMetadata[address(pool)];
 
-        uint32 secondsAgo = poolMetadata.oldestObservation;
+        uint32 secondsAgo = poolMetadata.maxSecondsAgo;
         if (secondsAgo > 1 days) secondsAgo = 1 days;
         // Throws if secondsAgo == 0
         (int24 arithmeticMeanTick, uint160 secondsPerLiquidityX128) = Oracle.consult(pool, secondsAgo);
@@ -87,7 +114,7 @@ contract VolatilityOracle is IVolatilityOracle {
         );
     }
 
-    function _pickReadIndex(IUniswapV3Pool pool, Volatility.FeeGrowthGlobals[26] memory feeGrowthGlobal)
+    function _pickReadIndex(IUniswapV3Pool pool, Volatility.FeeGrowthGlobals[25] memory feeGrowthGlobal)
         private
         view
         returns (uint8)
@@ -95,14 +122,14 @@ contract VolatilityOracle is IVolatilityOracle {
         uint8 readIndex = feeGrowthGlobalsReadIndex[address(pool)];
         uint32 timingError = _timingError(block.timestamp - feeGrowthGlobal[readIndex].timestamp);
 
-        for (uint8 counter = readIndex + 1; counter < readIndex + 26; counter++) {
-            uint8 newReadIndex = counter % 26;
+        for (uint8 counter = readIndex + 1; counter < readIndex + 25; counter++) {
+            uint8 newReadIndex = counter % 25;
             uint32 newTimingError = _timingError(block.timestamp - feeGrowthGlobal[newReadIndex].timestamp);
 
             if (newTimingError < timingError) {
                 readIndex = newReadIndex;
                 timingError = newTimingError;
-            }
+            } else break;
         }
 
         return readIndex;
--- contracts/interfaces/IVolatilityOracle.sol
+++ contracts/interfaces/IVolatilityOracle.sol
@@ -4,16 +4,71 @@ pragma solidity ^0.8.10;
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 interface IVolatilityOracle {
-    function cachedPoolMetadata(address)
+    /**
+     * @notice Accesses the most recently stored metadata for a given Uniswap pool
+     * @dev These values may or may not have been initialized and may or may not be
+     * up to date. `tickSpacing` will be non-zero if they've been initialized.
+     * @param pool The Uniswap pool for which metadata should be retrieved
+     * @return maxSecondsAgo The age of the oldest observation in the pool's oracle
+     * @return gamma0 The pool fee minus the protocol fee on token0, scaled by 1e6
+     * @return gamma1 The pool fee minus the protocol fee on token1, scaled by 1e6
+     * @return tickSpacing The pool's tick spacing
+     */
+    function cachedPoolMetadata(address pool)
         external
         view
         returns (
-            uint32 oldestObservation,
+            uint32 maxSecondsAgo,
             uint24 gamma0,
             uint24 gamma1,
             int24 tickSpacing
         );
 
+    /**
+     * @notice Accesses any of the 25 most recently stored fee growth structs
+     * @dev The full array (idx=0,1,2...24) has data that spans *at least* 24 hours
+     * @param pool The Uniswap pool for which fee growth should be retrieved
+     * @param idx The index into the storage array
+     * @return feeGrowthGlobal0X128 Total pool revenue in token0, as of timestamp
+     * @return feeGrowthGlobal1X128 Total pool revenue in token1, as of timestamp
+     * @return timestamp The time at which snapshot was taken and stored
+     */
+    function feeGrowthGlobals(address pool, uint256 idx) external view returns (
+        uint256 feeGrowthGlobal0X128,
+        uint256 feeGrowthGlobal1X128,
+        uint32 timestamp
+    );
+
+    /**
+     * @notice Returns the index that was closest to 24 hours old last time
+     * `estimate24H` was called
+     * @param pool The Uniswap pool for which read index should be fetched
+     * @return An index into `feeGrowthGlobals`
+     */
+    function feeGrowthGlobalsReadIndex(address pool) external view returns (uint8);
+
+    /**
+     * @notice Returns the index that was written to last time `estimate24H` was called
+     * @param pool The Uniswap pool for which write index should be fetched
+     * @return An index into `feeGrowthGlobals`
+     */
+    function feeGrowthGlobalsWriteIndex(address pool) external view returns (uint8);
+
+    /**
+     * @notice Updates cached metadata for a Uniswap pool. Must be called at least once
+     * in order for volatility to be determined. Should also be called whenever
+     * protocol fee changes
+     * @param pool The Uniswap pool to poke
+     */
+    function cacheMetadataFor(IUniswapV3Pool pool) external;
+
+    /**
+     * @notice Estimates 24-hour implied volatility for a Uniswap pool.
+     * @param pool The pool to use for volatility estimate
+     * @param sqrtPriceX96 The pool's current sqrtRatio, from pool.slot0()
+     * @param tick The pool's current tick, from pool.slot0()
+     * @return IV The estimated volatility, scaled by 1e18
+     */
     function estimate24H(
         IUniswapV3Pool pool,
         uint160 sqrtPriceX96,
