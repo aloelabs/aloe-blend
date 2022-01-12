31a8746c3b96dc071d24a0e823390eaa21205b7d
hayden Shively <haydenshively@gmail.com>
2022-01-12 16:47:10 -0600

    Update recenter() to retain float % in contract

--- contracts/interfaces/IAloeBlendEvents.sol
+++ contracts/interfaces/IAloeBlendEvents.sol
@@ -8,7 +8,7 @@ interface IAloeBlendEvents {
 
     event Rebalance(uint32 urgency, uint256 ratio, uint256 shares, uint256 inventory0, uint256 inventory1);
 
-    event Recenter(int24 lower, int24 upper, uint96 magic);
+    event Recenter(int24 lower, int24 upper);
 
     event Reward(address token, uint256 amount, uint32 urgency);
 }
